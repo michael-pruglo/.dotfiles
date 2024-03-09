befe53d219b445d0cb2083b7cac644fdf4028af0
michael.p <michael.pruglo.work@gmail.com>
2024-03-09 15:01:02 +0200

    cosmetics

--- nvim/lua/plugins/_niceties.lua
+++ nvim/lua/plugins/_niceties.lua
@@ -1,52 +1,65 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "visual feedback on yank",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function() vim.highlight.on_yank() end,
+    desc = "visual feedback on yank",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function() vim.highlight.on_yank() end,
 })
 
 return {
-	{
-		"catppuccin/nvim",
-		priority = 1000,
-		lazy = false,
-		name = "catppuccin",
-		config = function()
-			vim.cmd.colorscheme "catppuccin"
-		end
-	},
-	{
-		"nvim-tree/nvim-web-devicons",
-		opts = {}
-	},
-	{
-		-- maybe move to mini.pairs?
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {}
-	},
-	{
-		-- maybe move to mini.comment?
-		"numToStr/Comment.nvim",
-		opts = {}
-	},
-	{
-		"echasnovski/mini.nvim",
-		config = function()
-			require("mini.ai").setup { n_lines = 500 }
-			require("mini.surround").setup()
-		end
-	},
-	{
-		"tpope/vim-sleuth",
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {}
-	},
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+        lazy = false,
+        name = "catppuccin",
+        config = function()
+            vim.cmd.colorscheme "catppuccin"
+            require("catppuccin").setup({
+                integrations = {
+                    cmp = true,
+                    mason = true,
+                    neotree = true,
+                }
+            })
+        end
+    },
+    {
+        "nvim-tree/nvim-web-devicons",
+        opts = {}
+    },
+    {
+        -- maybe move to mini.pairs?
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {}
+    },
+    {
+        -- maybe move to mini.comment?
+        "numToStr/Comment.nvim",
+        opts = {}
+    },
+    {
+        "echasnovski/mini.nvim",
+        config = function()
+            require("mini.ai").setup { n_lines = 500 }
+            require("mini.surround").setup()
+        end
+    },
+    {
+        "tpope/vim-sleuth",
+    },
+    {
+        -- supposedly lazy and fast
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            options = {
+                theme = "catppuccin",
+                component_separators = { left="|", right="|" },
+            }
+        }
+    },
 
-	-- consider: 
-	--     lukas-reineke/indent-blankline.nvim -- vertical lines for indents
-	--     mini.sessions -- remember where you left off
+    -- consider: 
+    --     lukas-reineke/indent-blankline.nvim -- vertical lines for indents
+    --     mini.sessions -- remember where you left off
 }
 
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,8 +4,12 @@ vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
-vim.opt.scrolloff = 5
+vim.opt.scrolloff = 7
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevelstart = 99
 
