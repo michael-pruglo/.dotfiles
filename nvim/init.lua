3bd22f5696b9b29fa8c520a9c89f5dd5c2982aa6
michael.p <michael.pruglo.work@gmail.com>
2024-03-10 10:06:32 +0200

    update tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,5 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup("plugin")
 
